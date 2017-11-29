363b2c0756ce66de48da87697578dc359c21a866
Seika139 <tcoanzu@gmail.com>
2017-11-30 00:42:30 +0900

    っk

--- tweet01.py
+++ tweet01.py
@@ -17,7 +17,7 @@ t.statuses.update(status=status) #Twitterに投稿
 #画像付きツイート
 pic="udsuki2.jpg" #画像を投稿するなら画像のパス
 with open(pic,"rb") as image_file:
- image_data=image_file.read()
+    image_data=image_file.read()
 pic_upload = twitter.Twitter(domain='upload.twitter.com',auth=auth)
 id_img1 = pic_upload.media.upload(media=image_data)["media_id_string"]
 t.statuses.update(status=status,media_ids=",".join([id_img1]))
