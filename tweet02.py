fda89efd05aaadce0fa1d739f261b019837b1e32
Seika139 <tcoanzu@gmail.com>
2017-12-05 10:15:55 +0900

    tweet02.pyを追加

--- tweet02.py
+++ tweet02.py
@@ -1,24 +1,41 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+# coding: utf-8
 
-from datetime import *
-import twitter
-import os
+import json
+from requests_oauthlib import OAuth1Session
+
+AT = "863556291729301504-MaPF7pwhLQtFwUrtMIzxS0gQd7wEEOC"
+AS = "fCp80ufrYK6rMVI9lqC0SOiXE6kRlTj4T7lhw6aZdKDw1"
+CK = "fSMitUng7w3UkXU4kCMYrXmC6"
+CS = "SSsyuo4tE6RW8yQLAZJLLa9795NYcd4mppLnkEDHOcv6iuSIqU"
 
 
-now = datetime.today() - timedelta(0,60)
-text =  str(now.year) + u"年" + str(now.month) + u"月" + str(now.day) + u"日" + str(now.hour) + u"時" + str(now.minute) + u"分のTwitterトレンドです。"
+url_media = "https://upload.twitter.com/1.1/media/upload.json"
+url_text = "https://api.twitter.com/1.1/statuses/update.json"
 
-#print text
+# OAuth認証 セッションを開始
+twitter = OAuth1Session(CK, CS, AT, AS)
 
+# 画像投稿
+files = {"media" : open('/Users/suzukikenichi/test2/yui.jpg', 'rb')}
+req_media = twitter.post(url_media, files = files)
 
-token = "863556291729301504-MaPF7pwhLQtFwUrtMIzxS0gQd7wEEOC"
-token_secret = "fCp80ufrYK6rMVI9lqC0SOiXE6kRlTj4T7lhw6aZdKDw1"
-consumer_key = "fSMitUng7w3UkXU4kCMYrXmC6"
-consumer_secret = "SSsyuo4tE6RW8yQLAZJLLa9795NYcd4mppLnkEDHOcv6iuSIqU"
+# レスポンスを確認
+if req_media.status_code != 200:
+    print ("画像アップデート失敗: %s", req_media.text)
+    exit()
 
-auth = twitter.OAuthHandler(consumer_key,consumer_secret)
-auth.set_access_token(token,token_secret)
+# Media ID を取得
+media_id = json.loads(req_media.text)['media_id']
+print ("Media ID: %d" % media_id)
 
-api = twitter.API(auth)
+# Media ID を付加してテキストを投稿
+params = {'status': '画像送信テスト中', "media_ids": [media_id]}
+req_media = twitter.post(url_text, params = params)
 
-api.update_with_media(filename='./now.png',status=text)
+# 再びレスポンスを確認
+if req_media.status_code != 200:
+    print ("テキストアップデート失敗: %s", req_text.text)
+    exit()
+
+print ("OK")
