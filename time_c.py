c75d2273f3882bd33f7201fc0de0f0a7a67fe517
Seika139 <tcoanzu@gmail.com>
2018-02-09 19:50:56 +0900

    ９時間前ではなく９時間後にずらすのだ！

--- time_c.py
+++ time_c.py
@@ -1,6 +1,6 @@
 import datetime as dt
 xd = dt.datetime.today()
-xdj = xd - dt.timedelta(hours=9) #Herokuでは時間帯がUTCなので9時間前にずらす
+xdj = xd + dt.timedelta(hours=9) #Herokuでは時間帯がUTCなので9時間後にずらす
 xh = xd.hour
 xhj = xdj.hour
 print("今は",xh,"時")
